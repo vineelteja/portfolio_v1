a83c13ee293695fd31152930173fea64a8f99636
vineel <vineel>
2021-06-24 17:05:46 +0000

    changed repo address

--- src/mock/data.js
+++ src/mock/data.js
@@ -42,7 +42,7 @@ export const projectsData = [
     info: `While every restaurant is busy in providing good food to the customers, we are here to help you bring online presence to your restaurant.`,
     info2: `It is very easy to sign up on the website, upload photos and just add your menu items of the restaurant.`,
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/vineelteja/wc_project2', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
